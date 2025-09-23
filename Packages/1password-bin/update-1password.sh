23b2e2ff86949b905c9df15c328f53883ced7838
Okiki <hey@okikio.dev>
2025-09-23 19:59:29 -0400

    chore: update pkgs

--- Packages/1password-bin/update-1password.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# update-1password.sh
-NEW_VER="$1"
-[[ -z "$NEW_VER" ]] && { echo "Usage: $0 <version>"; exit 1; }
-
-cd ~/pkgbuilds/1password
-sed -i "s/^_tarver=.*/_tarver=${NEW_VER}/" PKGBUILD
-updpkgsums
-makepkg -c
