57446c08e01f17aaeeb7d62bcccc0a561e61a5db
Okiki Ojo <okiki@thunderstrike.co>
2026-04-08 23:04:23 -0400

    refactor(vesktop): improve configuration handling and user flags processing

--- packages/vesktop-git/vesktop.sh
+++ packages/vesktop-git/vesktop.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
 
-XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-~/.config}
-
-# Allow users to override command-line options
-if [ -f "$XDG_CONFIG_HOME/vesktop-flags.conf" ]; then
-    VESKTOP_USER_FLAGS="$(grep -v '^#' "$XDG_CONFIG_HOME/vesktop-flags.conf")"
+if [ -n "${XDG_CONFIG_HOME:-}" ]; then
+  config_home=$XDG_CONFIG_HOME
+else
+  config_home=$HOME/.config
 fi
 
-# Launch (each word in VESKTOP_USER_FLAGS must be split)
+read_flags() {
+  flags_file=$1
+
+  [ -f "$flags_file" ] || return 0
+
+  sed \
+    -e 's/[[:space:]]*$//' \
+    -e '/^[[:space:]]*#/d' \
+    -e '/^[[:space:]]*$/d' \
+    "$flags_file"
+}
+
+VESKTOP_USER_FLAGS=$(read_flags "$config_home/vesktop-flags.conf")
+
+set -f
 # shellcheck disable=SC2086
 exec /usr/lib/vesktop/vesktop $VESKTOP_USER_FLAGS "$@"
