7cd046adbd89c57f1eab99b967873459aa3703db
Okiki Ojo <okiki@thunderstrike.co>
2026-04-08 23:14:20 -0400

    fix: refactor vesktop.sh to improve user flags handling and configuration

--- packages/vesktop-electron-git/vesktop.sh
+++ packages/vesktop-electron-git/vesktop.sh
@@ -1,12 +1,35 @@
 #!/bin/sh
 
-XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-~/.config}
-
-# Allow users to override command-line options
-if [ -f "$XDG_CONFIG_HOME/vesktop-flags.conf" ]; then
-    VESKTOP_USER_FLAGS="$(grep -v '^#' "$XDG_CONFIG_HOME/vesktop-flags.conf")"
+if [ -n "${XDG_CONFIG_HOME:-}" ]; then
+  config_home=$XDG_CONFIG_HOME
+else
+  config_home=$HOME/.config
 fi
 
-# Launch (each word in VESKTOP_USER_FLAGS must be split)
-# shellcheck disable=SC2086
-exec electron /usr/lib/vesktop/app.asar $VESKTOP_USER_FLAGS "$@"
+read_flags() {
+  flags_file=$1
+
+  [ -f "$flags_file" ] || return 0
+
+  sed \
+    -e 's/[[:space:]]*$//' \
+    -e '/^[[:space:]]*#/d' \
+    -e '/^[[:space:]]*$/d' \
+    "$flags_file"
+}
+
+VESKTOP_USER_FLAGS=$(read_flags "$config_home/vesktop-flags.conf")
+
+export ELECTRON_IS_DEV=0
+export ELECTRON_FORCE_IS_PACKAGED=true
+export NODE_ENV=production
+
+set -f
+
+if [ "$(id -u)" -eq 0 ] && [ -z "${ELECTRON_RUN_AS_NODE:-}" ]; then
+  # shellcheck disable=SC2086
+  exec /usr/bin/electron /usr/lib/vesktop/app.asar --no-sandbox $VESKTOP_USER_FLAGS "$@"
+else
+  # shellcheck disable=SC2086
+  exec /usr/bin/electron /usr/lib/vesktop/app.asar $VESKTOP_USER_FLAGS "$@"
+fi
