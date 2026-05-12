4ec04eeb50a9d4082232bd01025a89ee7f23a670
Okiki Ojo <okiki@thunderstrike.co>
2026-05-12 14:16:59 -0400

    fix(electron-wayland-unpatch): remove shellcheck disable comment

--- packages/1password-wayland/electron-wayland-unpatch.sh
+++ packages/1password-wayland/electron-wayland-unpatch.sh
@@ -10,7 +10,7 @@ if [[ ! -L '{{target}}' ]]; then
   exit 0
 fi
 
-# shellcheck disable=SC2193 # these are just template placeholders
+# these are just template placeholders
 if [[ '{{executable}}' == *'/{{pkgname}}/'* ]]; then
   # Move original executable back into place
   mv -fv '{{executable}}' '{{target}}'
