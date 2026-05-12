0a1493cb26c521a0e9703a2e381547d0dd37a6cd
Randall <randall@openarsenal.com>
2026-05-12 14:53:18 -0700

    refactor(vesktop-bin): update build
    Normalize the prebuilt bundled-Electron package layout, dependencies, launcher wrapper, desktop entry, install hook, and metadata to match the Electron package conventions.

---
+++ packages/vesktop-bin/vesktop-flags-wrapper.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-"$HOME/.config"}
+
+read_flags() {
+  local file="$1"
+  local -n out="$2"
+  local line
+
+  out=()
+  [[ -f "$file" ]] || return 0
+
+  while IFS= read -r line || [[ -n "$line" ]]; do
+    line="${line%%#*}"
+    [[ -n "${line//[[:space:]]/}" ]] && out+=("$line")
+  done <"$file"
+}
+
+vesktop_flags=()
+platform_flags=()
+args=()
+
+read_flags "$XDG_CONFIG_HOME/vesktop-flags.conf" vesktop_flags
+
+for arg in "$@"; do
+  case "$arg" in
+    --wayland)
+      platform_flags+=("--ozone-platform=wayland")
+      ;;
+    --x11|--xwayland)
+      platform_flags+=("--ozone-platform=x11")
+      ;;
+    *)
+      args+=("$arg")
+      ;;
+  esac
+done
+
+exec @VESKTOP_EXEC@ "${vesktop_flags[@]}" "${platform_flags[@]}" "${args[@]}"
